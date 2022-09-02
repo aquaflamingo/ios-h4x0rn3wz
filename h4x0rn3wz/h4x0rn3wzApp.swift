666d0cbd3b31a990512404f875f6649cc5e65c97
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-02 13:45:50 -0400

    Initial Commit

---
+++ h4x0rn3wz/h4x0rn3wzApp.swift
@@ -0,0 +1,17 @@
+//
+//  h4x0rn3wzApp.swift
+//  h4x0rn3wz
+//
+//  Created by robertsimoes on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct h4x0rn3wzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
